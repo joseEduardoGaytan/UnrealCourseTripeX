3e008c6ad7ef8fbc5350507bf2605de8b4354fca
joseeduardogaytan <joseEduardoGaytan@gmail.com>
2020-04-17 17:58:02 -0500

    hello world

---
+++ TripleX.cpp
@@ -0,0 +1,9 @@
+// #include - Preposessor direcive instruction to the compiler used to include a library before compiling
+// we tell the compiler to include a library
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello Wrold!";
+    return 0;
+}
